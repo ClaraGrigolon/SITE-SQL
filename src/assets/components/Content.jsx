87311d25490017e20808f8456146dc0263a2301b
ClaraGrigolon <claragrigolon@gmail.com>
2025-03-26 10:52:36 -0300

    Add files via upload

---
+++ src/assets/components/Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Content.css';
+
+function Content(){
+    return(
+        <>
+            <section>
+                <h2>Fundamentos da linguagem SQL</h2>
+                <p>texto aqui</p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ncgEQBONI_w?si=FBnN89iG5lbRjMAY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </section>
+        </>
+    );
+};
+export default Content;
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return(
+        <>
+          <header>
+            <h1>SQL - Aprendendo o básico</h1>
+          </header>
+        </>
+    );
+};
+export default Header;
